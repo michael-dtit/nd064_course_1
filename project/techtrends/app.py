e4704cd1d048864bb153556509153cfa589db4ee
michaeldtit <michaeldtit@telekom.it>
2025-03-13 14:13:52 +0100

    update logging after review

--- project/techtrends/app.py
+++ project/techtrends/app.py
@@ -10,22 +10,33 @@ logging_config = {
     'formatters': {'default': {
         'format': '[%(asctime)s] %(levelname)s in %(module)s: %(message)s',
     }},
-    'handlers': {'stdout': {
+    'handlers': {'wsgi': {
+        'class': 'logging.StreamHandler',
+        'stream': 'ext://flask.logging.wsgi_errors_stream',
+        'formatter': 'default',
+        'level': 'ERROR'
+    },
+    'stdout': {
         'class': 'logging.StreamHandler',
         'stream': 'ext://sys.stdout',
-        'formatter': 'default'
+        'formatter': 'default',
+        'level': 'DEBUG'
     }},
     'root': {
         'level': 'DEBUG',
-        'handlers': ['stdout']
+        'handlers': ['wsgi', 'stdout']
     }
 }
+
+db_connection_count = 0
 
 # Function to get a database connection.
 # This function connects to database with the name `database.db`
 def get_db_connection():
     connection = sqlite3.connect('database.db')
     connection.row_factory = sqlite3.Row
+    global db_connection_count
+    db_connection_count += 1
     return connection
 
 # Function to get a post using its ID
@@ -54,17 +65,17 @@ def index():
 def post(post_id):
     post = get_post(post_id)
     if post is None:
-      app.logger.info("couldn't find post with id %d", post_id)
+      app.logger.error("couldn't find post with id %d", post_id)
       return render_template('404.html'), 404
     else:
       post_title = post[2]
-      app.logger.info("Article \"%s\" retrieved!", post_title)
+      app.logger.debug("Article \"%s\" retrieved!", post_title)
       return render_template('post.html', post=post)
 
 # Define the About Us page
 @app.route('/about')
 def about():
-    app.logger.info("The \"About Us\" page is retrieved.")
+    app.logger.debug("The \"About Us\" page is retrieved.")
     return render_template('about.html')
 
 # Define the post creation functionality 
@@ -77,7 +88,7 @@ def create():
         if not title:
             flash('Title is required!')
         else:
-            app.logger.info("A new article is created: \"%s\"", title)
+            app.logger.debug("A new article is created: \"%s\"", title)
             connection = get_db_connection()
             connection.execute('INSERT INTO posts (title, content) VALUES (?, ?)',
                          (title, content))
@@ -102,8 +113,9 @@ def metrics():
     connection = get_db_connection()
     posts = connection.execute('SELECT COUNT(1) FROM posts').fetchone()[0]
     connection.close()
+    global db_connection_count
     response = app.response_class(
-        response=json.dumps({"db_connection_count": 1, "post_count": posts}),
+        response=json.dumps({"db_connection_count": db_connection_count, "post_count": posts}),
         status=200,
         mimetype='application/json'
     )
